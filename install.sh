1566813db01aa95e1f7f66433f1468421cd7745a
SYKLO <amirgoles84@gmail.com>
2025-05-20 12:48:12 +0330

    remove redondent command in install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ sudo dnf -y config-manager setopt fedora-cisco-openh264.enabled=1
 yes | sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-sudo dnf -y check-update
 sudo dnf -y install code warp-cli gcc g++ make cmake git gh vim nvim btop fastfetch flatpak snapd
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
